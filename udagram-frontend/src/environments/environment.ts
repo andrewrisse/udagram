bea5fdd583246053f2293e9ee993482162b48955
Andrew Risse <andrewrisse@gmail.com>
2021-06-16 15:14:56 -0600

    Add deployment files

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aede3338e19094c87aa3fccaf8ecb711-472555739.us-west-2.elb.amazonaws.com/api/v0'
 };
 
 /*
